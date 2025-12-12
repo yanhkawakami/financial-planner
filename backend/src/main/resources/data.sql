d0f6b72d625fad173f898b6897477a86fc0475ae
Yan Hideki Kawakami <yanhkawakami@gmail.com>
2025-12-12 17:57:30 -0300

    fix: correcao da chamada para obter o access token

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -6,11 +6,15 @@ INSERT INTO tb_category (name) VALUES ('Entretenimento');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
 INSERT INTO tb_role (authority) VALUES ('ROLE_USER');
 
--- Users (senha: 123456)
-INSERT INTO tb_user (name, email, phone, password) VALUES ('Admin', 'admin@email.com', '11999999999', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
-INSERT INTO tb_user (name, email, phone, password) VALUES ('User', 'user@email.com', '11888888888', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+-- Users
+-- Admin: senha "123456"
+INSERT INTO tb_user (name, email, phone, password) VALUES ('Admin', 'admin@email.com', '11999999999', '$2a$10$NYFZ/8WaQ3Qb6FCs.00jce4nxX9w7AkgWVsQCG6oUwTAcZqP9Flqu');
+-- User: senha "123456"
+INSERT INTO tb_user (name, email, phone, password) VALUES ('User', 'user@email.com', '11888888888', '$2a$10$NYFZ/8WaQ3Qb6FCs.00jce4nxX9w7AkgWVsQCG6oUwTAcZqP9Flqu');
 
 -- User Roles
+-- Admin tem ROLE_ADMIN e ROLE_USER
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 2);
-INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+-- User tem apenas ROLE_USER
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
